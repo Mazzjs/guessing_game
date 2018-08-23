fbb5a01202eb7d5e6141a895ee6d79757082366b
mazz <mazzjs@gmail.com>
2018-08-23 13:02:53 +0800

    finx ignore conflicts

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("guess the number" );
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("the secret numbr is : {}", secret_number);
+    loop {
+        println!("place input your guess:)");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).ok().expect("fail to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!" ),
+            Ordering::Equal => {println!("you win" ); break;},
+        }
+    }
+}
